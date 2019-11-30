899335773f93b6c9b9241cf9b35dbe68084d0db6
Bryan <brayanvilla167@gmail.com>
2019-11-30 15:59:14 -0600

    Error en on_delete con models

--- lists/tests.py
+++ lists/tests.py
@@ -68,4 +68,23 @@ class ListViewTest(TestCase):
         Item.objects.create(text='itemey 2')
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertContains(response, 'itemey 1')  
-        self.assertContains(response, 'itemey 2') 
+        self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+        
